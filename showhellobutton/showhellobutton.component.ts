7cc22a1dc3b0279a8d99613bdb0e0c1ba2885b44
kibs <darkbladekiven@gmail.com>
2024-09-21 00:30:00 +0800

    #2 component name: showhellobutton

---
+++ showhellobutton/showhellobutton.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-showhellobutton',
+  templateUrl: './showhellobutton.component.html',
+  styleUrl: './showhellobutton.component.css'
+})
+export class ShowhellobuttonComponent {
+  
+  showMessage: boolean = false;
+
+  
+  toggleMessage() {
+    this.showMessage = !this.showMessage;
+  }
+}
